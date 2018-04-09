911bbde5d6ac98e000fc7bd303b67f5cdd9e0d7b
EOKruglov <ye.kruglov@yandex.ru>
2018-04-09 18:16:18 +0300

    2

--- Polinom/MyForm.h
+++ Polinom/MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "TPolinom.h"
 
 namespace Polinom {
 
@@ -15,6 +16,11 @@ namespace Polinom {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
+
+		TPolinom *t1, *t2, *res;
+		TMonom *m;
+		//int x, y, z;
+		//double coeff;
 		MyForm(void)
 		{
 			InitializeComponent();
@@ -34,6 +40,18 @@ namespace Polinom {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
 
 	private:
 		/// <summary>
@@ -48,12 +66,147 @@ namespace Polinom {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Enabled = false;
+			this->textBox1->Location = System::Drawing::Point(59, 77);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(345, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(56, 36);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(31, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"coeff";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(172, 36);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(14, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"X";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(268, 36);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(14, 13);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Y";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(370, 36);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(14, 13);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Z";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(100, 33);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(33, 20);
+			this->textBox2->TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(192, 33);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(33, 20);
+			this->textBox3->TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(288, 33);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(33, 20);
+			this->textBox4->TabIndex = 7;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(390, 33);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(33, 20);
+			this->textBox5->TabIndex = 8;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(447, 77);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(60, 23);
+			this->button1->TabIndex = 9;
+			this->button1->Text = L"Add";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(530, 77);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(60, 23);
+			this->button2->TabIndex = 10;
+			this->button2->Text = L"Delete";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(924, 494);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	m->coeff = Convert::ToDouble(textBox2->Text);
+	m->x = Convert::ToInt32(textBox3->Text);
+	m->y = Convert::ToInt32(textBox4->Text);
+	m->z = Convert::ToInt32(textBox5->Text);
+
+	t1->InsByOrder(*m);
+	string prt;
+
+	
+}
+};
 }
--- Polinom/TPolinom.h
+++ Polinom/TPolinom.h
@@ -1,1 +1,401 @@
 #pragma once
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+struct TLink {
+	T value;
+	TLink* pNext;
+};
+
+template <class T>
+class TList {
+protected:
+	TLink <T> *pFirst, *pLast, *pPrev, *pCurr, *pStop;
+	int size, pos;
+public:
+	TList() {
+		pFirst = pLast = pPrev = pCurr = pStop = NULL;
+		size = 0;
+		pos = -1;
+	}
+
+	virtual void InsFirst(T el) {
+		TLink<T> *tmp = new TLink<T>;
+		tmp->value = el;
+		if (size == 0) {
+			pFirst = pLast = pCurr = tmp;
+			tmp->pNext = pStop;
+		}
+		else {
+			tmp->pNext = pFirst;
+			pFirst = tmp;
+		}
+		pos++;
+		size++;
+	}
+
+	virtual void InsCurr(T el) {
+		if (pFirst == pCurr)
+			InsFirst(el);
+		else {
+			TLink <T> *tmp = new TLink<T>;
+			tmp->value = el;
+			tmp->pNext = pCurr;
+			pPrev->pNext = tmp;
+			pCurr = tmp;
+			size++;
+		}
+	}
+
+	virtual void InsLast(T el) {
+		if (pFirst == pStop)
+			InsFirst(el);
+		else {
+			TLink<T> *tmp = new TLink<T>;
+			tmp->value = el;
+			pLast->pNext = tmp;
+			tmp->pNext = pStop;
+			pLast = tmp;
+			size++;
+		}
+	}
+
+	T GetEl() {
+		return pCurr->value;
+	}
+
+	void reset() {
+		pCurr = pFirst;
+		pos = 0;
+		pPrev = pStop;
+	}
+
+	void goNext() {
+		if (pFirst->pNext == pStop)
+			pPrev = pFirst;
+		else {
+			pPrev = pCurr;
+			pCurr = pCurr->pNext;
+			pos++;
+		}
+	}
+
+	virtual void DelFirst() {
+		if (size == 1) {
+			delete pFirst;
+			pFirst = pLast = pPrev = pCurr = pStop;
+		}
+		else
+		{
+			TLink<T> *tmp = pFirst->pNext;
+			delete pFirst;
+			pFirst = tmp;
+		}
+		size--;
+		pos--;
+
+	}
+
+	virtual void DelCurr() {
+		if (pCurr == pFirst)
+			DelFirst();
+		else {
+			pCurr = pCurr->pNext;
+			delete pPrev->pNext;
+			pPrev->pNext = pCurr;
+			size--;
+		}
+	}
+
+	virtual void DelLast() {
+		if (pLast == pFirst)
+			DelFirst();
+		else {
+			for (reset(); !(pCurr->pNext == pStop); goNext()) {}
+			delete pLast;
+			pLast = pCurr = pPrev;
+			reset();
+			size--;
+			pos--;
+		}
+	}
+
+	bool IsEnd() {
+		return pCurr == pStop;
+	}
+
+	bool IsEmpty() {
+		return pFirst == pStop;
+	}
+
+	T operator[](int poz) {
+		for (reset(); !IsEnd(); goNext()) {
+			if (pos == m - 1)
+				return pCurr->value;
+		}
+	}
+
+	void Sort(T el) {
+		if (pFirst == pStop || el < pFirst->value) {
+			InsFirst(el);
+			return;
+		}
+		if (el >= pLast->value) {
+			InsLast(el);
+			return;
+		}
+		for (reset(); !IsEnd(); goNext()) {
+			if (pCurr->value > el) {
+				InsCurr(el);
+				return;
+			}
+		}
+	}
+
+};
+
+struct TMonom {
+	double coeff;
+	int x, y, z;
+};
+
+bool operator <(const TMonom& t1, const TMonom& t2) {
+	if (t1.x < t2.x)
+		return true;
+	if (t1.x > t2.x)
+		return false;
+	if (t1.x == t2.x) {
+		if (t1.y < t2.y)
+			return true;
+		if (t1.y > t2.y)
+			return false;
+		if (t1.y == t2.y) {
+			if (t1.z < t2.z)
+				return true;
+			if (t1.z >= t2.z)
+				return false;
+		}
+	}
+}
+
+
+bool operator >(const TMonom& t1, const TMonom& t2) {
+	if (t1.x < t2.x)
+		return false;
+	if (t1.x > t2.x)
+		return true;
+	if (t1.x == t2.x) {
+		if (t1.y < t2.y)
+			return false;
+		if (t1.y > t2.y)
+			return true;
+		if (t1.y == t2.y) {
+			if (t1.z <= t2.z)
+				return false;
+			if (t1.z > t2.z)
+				return true;
+		}
+	}
+}
+
+bool operator ==(const TMonom& t1, const TMonom& t2) {
+	if (t1.x == t2.x)
+		if (t1.y == t2.y)
+			if (t1.z == t2.z)
+				return true;
+			else
+				return false;
+		else
+			return false;
+	else
+		return false;
+}
+
+bool operator !=(const TMonom& t1, const TMonom& t2) {
+	if (t1.x == t2.x)
+		if (t1.y == t2.y)
+			if (t1.z == t2.z)
+				return false;
+			else
+				return true;
+		else
+			return true;
+	else
+		return true;
+}
+
+
+
+template <class T>
+class THeadList :public TList<T> {
+protected:
+	TLink<T> *pHead;
+public:
+	THeadList() :TList() {
+		pHead = new TLink<T>;
+		pStop = pHead;
+		pHead->pNext = pHead;
+	}
+
+	~THeadList() {
+		TList::~TList();
+		delete pHead;
+	}
+
+	void InsFirst(T a) {
+		TList::InsFirst(a);
+		pHead->pNext = pFirst;
+	}
+
+	void DelFirst() {
+		pHead->pNext = pFirst;
+	}
+};
+
+class TPolinom :public THeadList<TMonom> {
+public:
+	TPolinom() :THeadList() {
+		pHead->value.coeff = 0;
+		pHead->value.z = 1;
+	}
+
+	TPolinom(TMonom *tm, int size) :THeadList() {
+		pHead->value.coeff = 0;
+		pHead->value.z = 1;
+		for (int i = 0; i < size; i++)
+			InsLast(tm[i]);
+	}
+
+	void InsByOrder(const TMonom& tm) {
+		if (pFirst == pCurr)
+			InsFirst(tm);
+		for (reset(); !IsEnd(); goNext()) {
+			if (this->pCurr->value == tm) {
+				this->pCurr->value.coeff += tm.coeff;
+				if (this->pCurr->value.coeff == 0)
+					DelCurr();
+				return;
+			}
+			if (this->pCurr->value < tm) {
+				InsCurr(tm);
+				return;
+			}
+		}
+		InsLast(tm);
+	}
+
+	void operator+=(TPolinom& q) {
+		q.reset();
+		reset();
+		while (!q.IsEnd() || !IsEnd()) {
+			if (pCurr->value == q.pCurr->value) {
+				pCurr->value.coeff += q.pCurr->value.coeff;
+				if (pCurr->value.coeff == 0) {
+					DelCurr();
+					q.goNext();
+				}
+				else {
+					goNext();
+					q.goNext();
+				}
+			}
+			else
+				if (pCurr->value < q.pCurr->value) {
+					InsCurr(q.pCurr->value);
+					q.goNext();
+				}
+				else
+					goNext();
+		}
+	}
+
+	void operator*= (const int& n) {
+		if (n == 0)
+			for (reset(); !IsEnd(); goNext())
+				DelCurr();
+		for (reset(); !IsEnd(); goNext())
+			this->pCurr->value.coeff *= n;
+	}
+
+	void operator*= (const double& n) {
+		if (n == 0)
+			for (reset(); !IsEnd(); goNext())
+				DelCurr();
+		for (reset(); !IsEnd(); goNext())
+			this->pCurr->value.coeff *= n;
+	}
+
+	void operator*= (const TMonom& tm) {
+		for (reset(); !IsEnd(); goNext()) {
+			this->pCurr->value.coeff *= tm.coeff;
+			this->pCurr->value.x += tm.x;
+			this->pCurr->value.y += tm.y;
+			this->pCurr->value.y += tm.y;
+		}
+	}
+
+	void operator*= (TPolinom& tm) {
+		for (tm.reset(); !tm.IsEnd(); tm.goNext())
+			*this *= tm.pCurr->value;
+	}
+
+	bool operator==(TPolinom& tp) {
+		if (this->size != tp.size)
+			return false;
+		tp.reset();
+		reset();
+		while (!IsEnd()) {
+			if (this->pCurr->value != tp.pCurr->value)
+				return false;
+			goNext();
+			tp.goNext();
+		}
+		return true;
+	}
+
+	 string TPrint(TMonom& tm) {
+
+		int _x, _y, _z;
+		double _coeff;
+		string print;
+
+		_x = tm.x;
+		_y = tm.y;
+		_z = tm.z;
+		_coeff = tm.coeff;
+
+		
+		print = to_string(_coeff);
+		print += " * x^";
+		print += to_string(_x);
+		print += " * y^";
+		print += to_string(_y);
+		print += " * z^";
+		print += to_string(_z);
+
+		return print;
+
+	}
+
+
+	string TPrint(TPolinom& tp)
+	{
+		string tprint;
+
+		for (tp.reset(); !tp.IsEnd(); tp.goNext())
+		{
+			tprint += TPrint(tp.pCurr->value);
+		}
+		
+		return tprint;
+	}
+
+
+
+
+};
